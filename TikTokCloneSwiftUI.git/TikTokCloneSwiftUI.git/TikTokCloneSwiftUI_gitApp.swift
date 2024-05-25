d0932ffe70e46be51611790433030235e32d44da
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-05-25 11:49:57 +0300

    init project

---
+++ TikTokCloneSwiftUI.git/TikTokCloneSwiftUI.git/TikTokCloneSwiftUI_gitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokCloneSwiftUI_gitApp.swift
+//  TikTokCloneSwiftUI.git
+//
+//  Created by Oleksandr Isaiev on 25.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct TikTokCloneSwiftUI_gitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
