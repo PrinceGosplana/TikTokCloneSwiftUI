b1a8a3fe34b3b876c6b735b9b920527b5026f7ba
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-05-29 15:06:33 +0300

    create post model

---
+++ TikTokCloneSwiftUI.git/TikTokCloneSwiftUI.git/Model/Post.swift
@@ -0,0 +1,13 @@
+//
+//  Post.swift
+//  TikTokCloneSwiftUI.git
+//
+//  Created by Oleksandr Isaiev on 29.05.2024.
+//
+
+import Foundation
+
+struct Post: Identifiable, Codable {
+    let id: String
+    let videoUrl: String
+}
