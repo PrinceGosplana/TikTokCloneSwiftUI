68cb377e3b5fae0bbc4bb69a7a86d94514dbb9ca
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-06-01 09:18:27 +0300

    add resources

---
+++ TikTokCloneSwiftUI.git/TikTokCloneSwiftUI.git/Core/Authentication/View/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  TikTokCloneSwiftUI.git
+//
+//  Created by Oleksandr Isaiev on 01.06.2024.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
